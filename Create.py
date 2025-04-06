6a0fe203714a4b7ec97e2a7dbab9b2d7d3977fff
Tech God <106016855+techgod143@users.noreply.github.com>
2025-04-06 16:09:18 +0530

    Create Create.py
    Code by Tech God

---
+++ Create.py
@@ -0,0 +1,20 @@
+"""
+    @ code by ---( Tech God )---
+    @ Github : https://github.com/techgod143
+    @ YouTube : https://www.youtube.com/@techgod143
+    @ WhatsApp : https://whatsapp.com/channel/0029Va9Ufzi8kyyEnEHvOm1h
+    
+"""
+import os, sys,time, platform
+os.system('clear') 
+print('\033[0m [💸] \033[92m Follow Our WhatsApp channel For More Updates👿☠️ ') 
+time.sleep(3)
+os.system('xdg-open https://whatsapp.com/channel/0029Va9Ufzi8kyyEnEHvOm1h')
+print('\n\033[0m [\033[92m✓\033[97m] \033[92m Checking For Updates ....\n') 
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import Safeum
+elif bit == '32bit':
+    print("\033[0m\033[91m Sorry Your Device Is 32bit, This Tool 32bit Not Sported, Only For \033[92m64Bit\033[0m ]");exit() 
+ 
