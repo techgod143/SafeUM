d18a6fb56a06ddf0d9d31eb168028522b071843c
Tech God <106016855+techgod143@users.noreply.github.com>
2025-04-06 16:47:44 +0530

    Update Create.py

--- Create.py
+++ Create.py
@@ -1,20 +1,20 @@
 """
-    @ code by ---( Tech God )---
-    @ Github : https://github.com/techgod143
-    @ YouTube : https://www.youtube.com/@techgod143
-    @ WhatsApp : https://whatsapp.com/channel/0029Va9Ufzi8kyyEnEHvOm1h
+    @ code by ---( Younis john )---
+    @ Github : https://github.com/younis-dgk
+    @ WhatsApp : https://wa.me/923194999455
+    @ facebook : https://www.facebook.com/YounisDgk
     
 """
 import os, sys,time, platform
 os.system('clear') 
-print('\033[0m [💸] \033[92m Follow Our WhatsApp channel For More Updates👿☠️ ') 
+print('\033[0m [💸] \033[92m join Our WhatsApp group For More Updates 🥰✨') 
 time.sleep(3)
-os.system('xdg-open https://whatsapp.com/channel/0029Va9Ufzi8kyyEnEHvOm1h')
+os.system('xdg-open https://chat.whatsapp.com/CSfWIqJDSbJKdwLaQLXDFh')
 print('\n\033[0m [\033[92m✓\033[97m] \033[92m Checking For Updates ....\n') 
 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import SafeUM
+    import Safeum
 elif bit == '32bit':
     print("\033[0m\033[91m Sorry Your Device Is 32bit, This Tool 32bit Not Sported, Only For \033[92m64Bit\033[0m ]");exit() 
  
