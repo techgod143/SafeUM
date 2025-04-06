f998e7e28ee5c6bfb7ada9a97bebef4c9f195b33
Tech God <106016855+techgod143@users.noreply.github.com>
2025-04-06 16:46:32 +0530

    Update Create.py

--- Create.py
+++ Create.py
@@ -14,7 +14,7 @@ print('\n\033[0m [\033[92m✓\033[97m] \033[92m Checking For Updates ....\n')
 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import Safeum
+    import SafeUM
 elif bit == '32bit':
     print("\033[0m\033[91m Sorry Your Device Is 32bit, This Tool 32bit Not Sported, Only For \033[92m64Bit\033[0m ]");exit() 
  
